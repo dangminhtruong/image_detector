28b7208891855b42af49d85103ac0d8b78689958
dangminhtruong <xdangminhtruongx@gmail.com>
2017-12-11 14:38:28 +0700

    online db

--- routes/save_data.js
+++ routes/save_data.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/csdl_dpt";
+var url = "mongodb://root:789852@ds135196.mlab.com:35196/csdl_dpt";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
